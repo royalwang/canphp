ba717d1cdc78a7cfa3be794248f3c549a7c743f4
404352772@qq.com <404352772@qq.com@bc8eb3ef-b134-467a-9d50-e2080d1e60b7>
2013-11-30 09:51:46 +0000

    删除多余的文件
    git-svn-id: http://code.taobao.org/svn/canphp/trunk@3 bc8eb3ef-b134-467a-9d50-e2080d1e60b7

--- cpapp/protected/apps/appmanage/code/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'APP_STATE' => 1,
-  'APP_NAME' => '示例页面',
-  'APP_VER' => '1.0.2012.0820',
-  'APP_AUTHOR' => '单骑',
-  'APP_ORIGINAL_PREFIX' => '',
-  'APP_TABLES' => '',
-);
--- cpapp/protected/apps/appmanage/code/model/demoModel.php
+++
@@ -1,4 +0,0 @@
-<?php
-class demoModel extends baseModel{
-	protected $table = 'demo'; //设置表名
-}
