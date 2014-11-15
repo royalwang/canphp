3363e139fd02080375c5b3c06fdc693d9e1f786e
404352772@qq.com <404352772@qq.com@bc8eb3ef-b134-467a-9d50-e2080d1e60b7>
2014-11-15 06:31:50 +0000

    git-svn-id: http://code.taobao.org/svn/canphp/trunk@25 bc8eb3ef-b134-467a-9d50-e2080d1e60b7

--- cpapp/protected/apps/default/controller/indexController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class indexController extends baseController{
-	public function index(){
-		$this->title = model('default')->getTitle();
-		$this->hello = model('default')->getHello();
-		$this->display();
-	}
-}
