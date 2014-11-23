646f9d9dd2da4700d0394e0c7b5c44dc296cff3b
404352772@qq.com <404352772@qq.com@bc8eb3ef-b134-467a-9d50-e2080d1e60b7>
2014-11-23 14:21:51 +0000

    git-svn-id: http://code.taobao.org/svn/canphp/trunk@30 bc8eb3ef-b134-467a-9d50-e2080d1e60b7

---
+++ cpapp/protected/config/db.php
@@ -0,0 +1,14 @@
+<?php 
+return array (
+  'default' => 
+		  array (
+			'DB_TYPE' => 'mysql',
+			'DB_HOST' => 'localhost',
+			'DB_USER' => 'root',
+			'DB_PWD' => '123456',
+			'DB_PORT' => '3306',
+			'DB_NAME' => '123',
+			'DB_CHARSET' => 'utf8',
+			'DB_PREFIX' => 'cp_',
+		  ),
+);
---
+++ cpapp/protected/config/global.php
@@ -0,0 +1,30 @@
+<?php 
+return array (
+  'REWRITE' => 
+  array (
+  ),
+  'APP' => 
+  array (
+    'DEBUG' => true,
+    'LOG_ON' => false,
+    'LOG_PATH' => BASE_PATH . 'cache/log/',
+    'URL_HTTP_HOST' => '',
+  ),
+  'DB' => 
+  array (
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_USER' => 'root',
+    'DB_PWD' => '123456',
+    'DB_PORT' => '3306',
+    'DB_NAME' => '123',
+    'DB_CHARSET' => 'utf8',
+    'DB_PREFIX' => 'cp_',
+  ),
+  'TPL' => 
+  array (
+    'TPL_TEMPLATE_SUFFIX' => '.php',
+    'TPL_CACHE_ON' => true,
+    'TPL_CACHE_PATH' => BASE_PATH . 'cache/tpl_cache/',
+  ),
+);
