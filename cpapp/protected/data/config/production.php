4468e1f3dd8e2255814fcf53f18ce581140338fc
404352772@qq.com <404352772@qq.com@bc8eb3ef-b134-467a-9d50-e2080d1e60b7>
2014-11-28 14:24:47 +0000

    git-svn-id: http://code.taobao.org/svn/canphp/trunk@68 bc8eb3ef-b134-467a-9d50-e2080d1e60b7

--- cpapp/protected/data/config/production.php
+++ cpapp/protected/data/config/production.php
@@ -1,7 +1,21 @@
 <?php 
 return array (
-  'default' => 
-		  array (
+	//错误调试与日志配置
+	'DEBUG' => false,	//是否开启调试模式
+	'LOG_ON' => true,	//是否开启出错信息保存到文件
+	'LOG_PATH' => BASE_PATH . 'data/log/', //日志存放目录
+	'ERROR_URL' => '', //出错跳转地址
+		
+	//网址与路由配置
+	'URL_BASE' => '/', //设置网址域名				
+	'URL_REWRITE' =>array(
+	
+	),
+	
+	//数据库配置
+	'DB'=>array(
+		'default' => 
+			array (
 			'DB_TYPE' => 'mysql',
 			'DB_HOST' => 'localhost',
 			'DB_USER' => 'root',
@@ -10,5 +24,6 @@ return array (
 			'DB_NAME' => '123',
 			'DB_CHARSET' => 'utf8',
 			'DB_PREFIX' => 'cp_',
-		  ),
+			),
+		),
 );
