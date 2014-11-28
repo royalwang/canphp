604135e5d642e8c9ad6dade4ebbc368c11cb6adb
404352772@qq.com <404352772@qq.com@bc8eb3ef-b134-467a-9d50-e2080d1e60b7>
2014-11-28 14:10:53 +0000

    git-svn-id: http://code.taobao.org/svn/canphp/trunk@66 bc8eb3ef-b134-467a-9d50-e2080d1e60b7

--- cpapp/protected/data/config/global.php
+++ cpapp/protected/data/config/global.php
@@ -10,17 +10,7 @@ return array (
     'LOG_PATH' => BASE_PATH . 'cache/log/',
     'URL_HTTP_HOST' => '',
   ),
-  'DB' => 
-  array (
-    'DB_TYPE' => 'mysql',
-    'DB_HOST' => 'localhost',
-    'DB_USER' => 'root',
-    'DB_PWD' => '123456',
-    'DB_PORT' => '3306',
-    'DB_NAME' => '123',
-    'DB_CHARSET' => 'utf8',
-    'DB_PREFIX' => 'cp_',
-  ),
+
   'TPL' => 
   array (
     'TPL_TEMPLATE_SUFFIX' => '.php',
