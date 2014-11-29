8b823cadd9789cd220d230f3fcdd99cba62eb01d
404352772@qq.com <404352772@qq.com@bc8eb3ef-b134-467a-9d50-e2080d1e60b7>
2014-11-29 05:51:03 +0000

    git-svn-id: http://code.taobao.org/svn/canphp/trunk@73 bc8eb3ef-b134-467a-9d50-e2080d1e60b7

---
+++ cpapp/protected/app/base/controller/ErrorController.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\base\controller;
+class ErrorController extends \framework\base\Controller{
+
+	public function error403($e=null){
+	
+	}
+	
+	public function error404($e=null){
+	
+	}
+	
+	public function error500($e=null){
+	
+	}
+	
+	public function error($e=null){
+	
+	}
+}
